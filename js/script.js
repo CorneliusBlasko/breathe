d34cdd8c4d3cc3e3af2c63b8729a241a60d776e9
Alberto <albertomutcirilo@gmail.com>
2020-07-10 08:16:26 +0200

    Added support for english language

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+/* STRINGS */
+
+let breatheIn = 'Inspira';
+let hold = 'Aguanta la respiración...';
+let breatheOut = 'Espira';
+let themeSelector = 'Selecciona el fondo que más te guste';
+
+let breatheInES = 'Inspira';
+let holdES = 'Aguanta la respiración';
+let breatheOutES = 'Espira';
+let hidePointerES = 'Oculta la bola';
+let showPointerES = 'Muestra la bola';
+let themeSelectorES = 'Selecciona el fondo que más te guste';
+
+let breatheInEN = 'Breathe In';
+let holdEN = 'Hold...';
+let breatheOutEN = 'Breathe out';
+let hidePointerEN = 'Hide the ball';
+let showPointerEN = 'Show the ball';
+let themeSelectorEN = 'Select your favorite background';
+
 const container = document.getElementById('container');
 const text = document.getElementById('text');
 
@@ -7,12 +28,15 @@ const holdTime = totalTime / 5;
 
 const audio = document.getElementById('myAudio');
 let pointer = document.getElementById('pointer');
+let language = 'ES';
 
 let themesModal = document.getElementById('themesModal');
 let menuButton = document.getElementById('menuButton');
 let body = document.getElementById('body');
+let languageButton = document.getElementById('languageButton');
 
 menuButton.addEventListener('click', openBackgroundSelector);
+languageButton.addEventListener('click', changeLanguage);
 window.addEventListener('click', clickOutside);
 
 function openBackgroundSelector() {
@@ -28,14 +52,14 @@ function clickOutside(e) {
 breathAnimation();
 
 function breathAnimation() {
-  text.innerText = 'Inspira';
+  text.innerText = breatheIn;
   container.className = 'container grow';
 
   setTimeout(() => {
-    text.innerText = 'Aguanta la respiración';
+    text.innerText = hold;
 
     setTimeout(() => {
-      text.innerText = 'Espira';
+      text.innerText = breatheOut;
       container.className = 'container shrink';
     }, holdTime);
   }, breatheTime);
@@ -46,22 +70,14 @@ setInterval(breathAnimation, totalTime);
 function hideCircle() {
   if (document.getElementById('pointer').style.visibility == 'hidden') {
     document.getElementById('pointer').style.visibility = 'visible';
-    document.getElementById('hideCircle').innerText = 'Oculta la bola';
+    document.getElementById('hideCircle').innerText = changePointerText(true);
   } else {
     document.getElementById('pointer').style.visibility = 'hidden';
-    document.getElementById('hideCircle').innerText = 'Muestra la bola';
+    document.getElementById('hideCircle').innerText = changePointerText(false);
   }
 }
 
 $('button').on('click tap', playPause);
-
-function startStop(e) {
-  if (document.getElementById('startStop').innerText == 'Empieza') {
-    document.getElementById('startStop').innerText = 'Para';
-  } else {
-    document.getElementById('startStop').innerText = 'Empieza';
-  }
-}
 
 function playPause(e) {
   $(this).find('.fa').toggleClass('fa, fa-pause fa, fa-play');
@@ -75,100 +91,74 @@ function playPause(e) {
   }
 }
 
-function changeTheme(id) {
-  document.body.style.backgroundImage = "url('./img/" + id + ".jpg')";
-  if (id == '02' || id == '08') {
-    //RED
-    document
-      .getElementById('gradient')
-      .classList.remove('gradient-circle-blue');
-    document
-      .getElementById('gradient')
-      .classList.remove('gradient-circle-grey');
-    document
-      .getElementById('gradient')
-      .classList.remove('gradient-circle-pink');
-    document
-      .getElementById('gradient')
-      .classList.remove('gradient-circle-green');
-    document.getElementById('circle').classList.remove('circle-blue');
-    document.getElementById('circle').classList.remove('circle-pink');
-    document.getElementById('circle').classList.remove('circle-grey');
-    document.getElementById('circle').classList.remove('circle-green');
-    document.getElementById('gradient').classList.add('gradient-circle-red');
-    document.getElementById('circle').classList.add('circle-red');
-  } else if (id == '04' || id == '07') {
-    //BLUE
-    document.getElementById('gradient').classList.remove('gradient-circle-red');
-    document
-      .getElementById('gradient')
-      .classList.remove('gradient-circle-grey');
-    document
-      .getElementById('gradient')
-      .classList.remove('gradient-circle-pink');
-    document
-      .getElementById('gradient')
-      .classList.remove('gradient-circle-green');
-    document.getElementById('circle').classList.remove('circle-red');
-    document.getElementById('circle').classList.remove('circle-pink');
-    document.getElementById('circle').classList.remove('circle-grey');
-    document.getElementById('circle').classList.remove('circle-green');
-    document.getElementById('gradient').classList.add('gradient-circle-blue');
-    document.getElementById('circle').classList.add('circle-blue');
-  } else if (id == '03' || id == '09') {
-    //GREY
-    document.getElementById('gradient').classList.remove('gradient-circle-red');
-    document
-      .getElementById('gradient')
-      .classList.remove('gradient-circle-blue');
-    document
-      .getElementById('gradient')
-      .classList.remove('gradient-circle-pink');
-    document
-      .getElementById('gradient')
-      .classList.remove('gradient-circle-green');
-    document.getElementById('circle').classList.remove('circle-red');
-    document.getElementById('circle').classList.remove('circle-blue');
-    document.getElementById('circle').classList.remove('circle-pink');
-    document.getElementById('circle').classList.remove('circle-green');
-    document.getElementById('gradient').classList.add('gradient-circle-grey');
-    document.getElementById('circle').classList.add('circle-grey');
-  } else if (id == '01' || id == '05' || id == '06') {
-    //PINK
-    document.getElementById('gradient').classList.remove('gradient-circle-red');
-    document
-      .getElementById('gradient')
-      .classList.remove('gradient-circle-blue');
-    document
-      .getElementById('gradient')
-      .classList.remove('gradient-circle-grey');
-    document
-      .getElementById('gradient')
-      .classList.remove('gradient-circle-green');
-    document.getElementById('circle').classList.remove('circle-red');
-    document.getElementById('circle').classList.remove('circle-blue');
-    document.getElementById('circle').classList.remove('circle-grey');
-    document.getElementById('circle').classList.remove('circle-green');
-    document.getElementById('gradient').classList.add('gradient-circle-pink');
-    document.getElementById('circle').classList.add('circle-pink');
-  } else if (id == '10') {
-    //GREEN
-    document.getElementById('gradient').classList.remove('gradient-circle-red');
-    document
-      .getElementById('gradient')
-      .classList.remove('gradient-circle-blue');
-    document
-      .getElementById('gradient')
-      .classList.remove('gradient-circle-grey');
-    document
-      .getElementById('gradient')
-      .classList.remove('gradient-circle-pink');
-    document.getElementById('circle').classList.remove('circle-pink');
-    document.getElementById('circle').classList.remove('circle-blue');
-    document.getElementById('circle').classList.remove('circle-red');
-    document.getElementById('circle').classList.remove('circle-grey');
-    document.getElementById('gradient').classList.add('gradient-circle-green');
-    document.getElementById('circle').classList.add('circle-green');
+function changeLanguage() {
+  if (language == 'ES') {
+    language = 'EN';
+    changeCircleText(language);
+    document.getElementById('themeSelectorLabel').innerText = themeSelectorEN;
+    setHideLabelLanguage(language);
+  } else {
+    language = 'ES';
+    changeCircleText(language);
+    document.getElementById('themeSelectorLabel').innerText = themeSelectorES;
+    setHideLabelLanguage(language);
   }
-  themesModal.style.display = 'none';
+}
+
+function changePointerText(hidden) {
+  if (hidden) {
+    if (language == 'ES') {
+      return hidden ? hidePointerES : showPointerES;
+    } else {
+      return hidden ? hidePointerEN : showPointerEN;
+    }
+  } else {
+    if (language == 'ES') {
+      return hidden ? hidePointerES : showPointerES;
+    } else {
+      return hidden ? hidePointerES : showPointerEN;
+    }
+  }
+}
+
+function setHideLabelLanguage(language) {
+  if (language == 'ES') {
+    if (document.getElementById('pointer').style.visibility == 'visible') {
+      document.getElementById('hideCircle').innerText = showPointerES;
+    } else {
+      document.getElementById('hideCircle').innerText = hidePointerES;
+    }
+  } else {
+    if (document.getElementById('pointer').style.visibility == 'visible') {
+      document.getElementById('hideCircle').innerText = showPointerEN;
+    } else {
+      document.getElementById('hideCircle').innerText = hidePointerEN;
+    }
+  }
+}
+
+function changeCircleText(language) {
+  if (language == 'ES') {
+    if (text.innerText == breatheIn) {
+      text.innerText = breatheInES;
+    } else if (text.innerText == hold) {
+      text.innerText = holdES;
+    } else if (text.innerText == breatheOut) {
+      text.innerText = breatheOutES;
+    }
+    hold = holdES;
+    breatheOut = breatheOutES;
+    breatheIn = breatheInES;
+  } else if (language == 'EN') {
+    if (text.innerText == breatheIn) {
+      text.innerText = breatheInEN;
+    } else if (text.innerText == hold) {
+      text.innerText = holdEN;
+    } else if (text.innerText == breatheOut) {
+      text.innerText = breatheOutEN;
+    }
+    hold = holdEN;
+    breatheOut = breatheOutEN;
+    breatheIn = breatheInEN;
+  }
 }
